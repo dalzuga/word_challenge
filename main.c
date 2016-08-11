8c1b5d6781e810b7ff624c1ccd998e3670f709ed
Daniel Alzugaray <dalzuga@gmail.com>
2016-08-11 18:28:01 +0000

    words now stored in a WordList

--- main.c
+++ main.c
@@ -18,13 +18,17 @@ typedef struct WordList {
 
 int main(void)
 {
-	WordList *word;
+	WordList *word, *first_node;
 	char c = '\0';
 	char buf[BUFFER] = "";
 	/* char *str; */
 	int i = 0, n = 0;
 
 	word = malloc(sizeof(WordList));
+	word->str = NULL;
+	word->next = NULL;
+	word->n = 0;
+	first_node = word;
 
 	while (c != EOF)
 	{
@@ -51,9 +55,26 @@ int main(void)
 		/* 	printf("str[%d] is:\t%c\n", i, str[i]); */
 		/* } */
 
-		printf("%s\n", word->str);
+		/* printf("%s\n", word->str); */
 		memset(buf, '\0', n); /* clear the buffer */
+
+		word->next = malloc(sizeof(WordList));
+		word = word->next;
+		word->str = NULL;
+		word->next = NULL;
+		word->n = 0;
 	}
 	
+	/* prints */
+	word = first_node;
+
+	while (word->next != NULL)
+	{
+		printf("%s\n", word->str);
+		word = word->next;
+	}
+
+	free(first_node);
+
 	return 0;
 }
